e4418fcb27b6399e7573b8258095e1b923979dd2
Orel Chalfon <Orelchalfon12@gmail.com>
2023-11-16 21:29:59 +0200

    Adding navigation to UsersPage

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,20 @@ import UsersPlacesPage from "./place/pages/UsersPlacesPage";
 import NewPlacePage from "./place/pages/NewPlacePage";
 import UpdatePlacePage from "./place/pages/UpdatePlacePage";
 import AuthenticatePage from "./user/pages/AuthenticatePage";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => navigate("/"), []);
   return (
     <>
       <MainNavigation />
       <main>
         <Routes>
           {/* <Route path="/users" exact element={WelcomeScreen} /> */}
-          <Route path="/" exact  element={<UsersPage />} />
+          <Route path="/" exact element={<UsersPage />} />
           <Route path="/:uId/places" exact element={<UsersPlacesPage />} />
           <Route path="/places/new" exact element={<NewPlacePage />} />
           <Route path="/places/:placeId" exact element={<UpdatePlacePage />} />
