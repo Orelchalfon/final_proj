aeaa5f6fd0dccb35279778b282095ae394a0fe17
Orel Chalfon <orelchalfon12@gmail.com>
2024-02-06 23:35:04 +0200

    Add .env file and update CSS styles***
    ***Add prop-types package***
    
    ***Update Vite configuration***
    
    ***Fix input label styling***
    
    ***Fix user count display***
    
    ***Update BackDrop component***
    
    ***Update UserItem component***
    
    ***Update Input component CSS

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/final_proj/",
-  plugins: [react()],
-})
+  base: "/",
+  plugins: [
+    react(),
+
+    
+
+    
+
+  ],
+});
